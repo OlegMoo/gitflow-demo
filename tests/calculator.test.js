7fc728e2c61c1d3e4eedf1ce914e07428033c437
Oleg Moroz <oleh.moroz@npp.nau.edu.ua>
2026-03-23 12:11:21 +0200

    Add multiply & subtract functions

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,5 +1,10 @@
 const { add } = require('../src/calculator');
+const { multiply } = require('../src/calculator');
 
 test('adds numbers', () => {
     expect(add(2,3)).toBe(5);
+});
+
+test('multiply numbers', () => {
+    expect(multiply(3,2)).toBe(6);
 });
